628e91978ca9bed218d656fa4e89547fb31e4d90
emmaddo <ermsapp@gmail.com>
2025-05-12 11:53:01 +0100

    A siimple way to return the highest number in an array of numbers using reduce()

--- src/app/BasicFilter/page.tsx
+++ src/app/BasicFilter/page.tsx
@@ -1,14 +1,20 @@
 "use client";
- import React from 'react'
-const a: number[] = [6, 2, 3, 4, 3, 5, 1];
+import React from 'react';
 
-const maxNumber = a.reduce((accumulator: number, currentValue: number)=>{
-return Math.max(accumulator, currentValue);
-});
+const arrayOfNumbers: number[] = [5, 3, 2, 4, 9, 5];
 
-console.log(maxNumber);
- export default function playPlay(){
-    return(
-        <div></div>
-    );
- }
+const highestNumber: number = arrayOfNumbers.reduce((initialValue: number, currentValue: number)=>{
+    if(currentValue > initialValue){
+            return currentValue;
+         }
+    else{
+            return initialValue;
+         }
+} )
+
+export default function playWithReduce(){
+    return(<div className='flex flex-col bg-gray-400 items-center justify-between p-4 m-4'>
+        <p className='text-white text-2xl p-2 m-2'>{`The higest number is ${highestNumber}`}</p>
+    </div>);
+    
+}
