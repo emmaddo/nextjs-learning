41897c66c533dfae4111833376a6d2bcd2ab8faf
emmaddo <ermsapp@gmail.com>
2025-05-12 12:07:15 +0100

    simple way to return element with the longest string in an array of string

---
+++ src/app/Practice/page.tsx
@@ -0,0 +1,22 @@
+"use client";
+import React from 'react';
+
+const stringArray: string[] = ['Prince', 'Rhema', 'Debora'];
+
+const longestString: string = stringArray.reduce((initialValue: string, currentValue: string) =>{
+    if(currentValue.length > initialValue.length){
+        return currentValue;
+    }
+    else{
+        return initialValue;
+    }
+}
+
+, ''); 
+
+export default function playWithReduce(){
+    return(<div className='flex flex-col bg-gray-400 items-center justify-between p-4 m-4'>
+        <p className='text-white text-2xl p-2 m-2'>{`The element with the longest string is ${longestString}`}</p>
+    </div>);
+    
+}
