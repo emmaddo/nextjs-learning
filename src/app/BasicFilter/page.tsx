47793587bf5e9a929dd869b74e2c054ef11ea2f8
emmaddo <ermsapp@gmail.com>
2025-05-12 11:56:36 +0100

    simple way to return the smallest number in an array of numbers using rereduce()

--- src/app/BasicFilter/page.tsx
+++ src/app/BasicFilter/page.tsx
@@ -1,20 +1,20 @@
 "use client";
 import React from 'react';
 
-const arrayOfNumbers: number[] = [5, 3, 2, 4, 9, 5];
+const arrayOfNumbers: number[] = [5, 3, 3, 2, 4, 9, 5];
 
-const highestNumber: number = arrayOfNumbers.reduce((initialValue: number, currentValue: number)=>{
-    if(currentValue > initialValue){
-            return currentValue;
-         }
+const lowestNumber: number = arrayOfNumbers.reduce((initialValue: number, currentValue: number)=>{
+    if(initialValue < currentValue){
+        return initialValue
+    }
     else{
-            return initialValue;
-         }
+        return currentValue;
+    }
 } )
 
 export default function playWithReduce(){
     return(<div className='flex flex-col bg-gray-400 items-center justify-between p-4 m-4'>
-        <p className='text-white text-2xl p-2 m-2'>{`The higest number is ${highestNumber}`}</p>
+        <p className='text-white text-2xl p-2 m-2'>{`The lowest number is ${lowestNumber}`}</p>
     </div>);
     
 }
